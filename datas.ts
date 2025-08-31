69ddcdeb57ff692360b1d0bcb408125276af8dc4
max <89175221282@mail.ru>
2025-08-31 22:24:01 +0300

    write financical_indicators json

---
+++ datas.ts
@@ -0,0 +1,34 @@
+const data = [
+{
+    period: {
+    year: 2024,
+    month: 1,
+    initial_amount:4546546
+  },
+  spending:[
+    {
+        id: 46546,
+         amount: 445646
+    }
+  ]
+
+}
+]
+interface moneyObject{
+    id: number|string,
+    name: string,
+    amount: number|string,
+    data?: number|string,
+  }
+
+
+interface financialIndicators {
+  period:{
+       year: number|string,
+    month: number|string,
+    initial_amount: number|string
+  },
+  spending : moneyObject[],
+  replenishment: moneyObject[]
+}
+
